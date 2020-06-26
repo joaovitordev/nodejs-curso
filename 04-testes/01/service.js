54194eac614c33f66de9b0114d3456216acd95c3
João Vitor <jv17fc@gmail.com>
2020-06-26 15:37:37 -0300

    Usando reduce e começando testes automatizados

---
+++ 04-testes/01/service.js
@@ -0,0 +1,15 @@
+const {
+    get
+} = require('axios')
+
+const URL = `https://swapi.dev/api/people`
+
+async function obterPessoas(nome) {
+    const url = `${URL}/?search=${nome}&format=json`
+    const result = await get(url)
+    return result.data
+}
+
+module.exports = {
+    obterPessoas
+}
