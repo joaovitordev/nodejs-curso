b0147f7f32a4cfa1481b8d0a92a45739aa2e8253
João Vitor <jv17fc@gmail.com>
2020-06-25 22:19:43 -0300

    consumindo api star wars com for

---
+++ 02-events/index.js
@@ -0,0 +1,19 @@
+const eventEmitter = require('events');
+const { EventEmitter } = require('events');
+const { normalize } = require('path');
+class MeuEmissor extends EventEmitter {
+
+}
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+// meuEmissor.emit(nomeEvento, 'no ok')
+
+// let count = 0
+// setInterval(function () {
+//     meuEmissor.emit(nomeEvento, 'no ok ' + (count++))
+// }, 1000)
