8d73622c8f2d43371d3805e99efce79610b8c860
João Vitor <jv17fc@gmail.com>
2020-06-25 22:28:46 -0300

    consumindo api star wars com for, forin e forof

--- 03-lists/for-forin/1.for-forin-forof.js
+++ 03-lists/for-forin/1.for-forin-forof.js
@@ -4,12 +4,25 @@ async function main() {
     try {
         const result = await service.obterPessoas('a')
         const names = []
-        console.time('for')
-        for (let i = 0; i <= result.results.length - 1; i++) {
-            const pessoa = result.results[i]
+        // console.time('for')
+        // for (let i = 0; i <= result.results.length - 1; i++) {
+        //     const pessoa = result.results[i]
+        //     names.push(pessoa.name)
+        // }
+        // console.timeEnd('for')
+
+        // console.time('forin')
+        // for (let i in result.results) {
+        //     const pessoa = result.results[i]
+        //     names.push(pessoa.name)
+        // }
+        // console.timeEnd('forin')
+
+        console.time('forof')
+        for (pessoa of result.results) {
             names.push(pessoa.name)
         }
-        console.timeEnd('for')
+        console.timeEnd('forof')
 
         console.log(`names`, names)
     } catch (error) {
